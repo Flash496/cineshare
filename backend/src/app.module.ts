e3069f972112378f4021b10d41f4d24225716b7a
flash496 <dbaul496@gmail.com>
2025-10-24 11:48:29 +0530

    Initial commit: Project setup with Next.js + NestJS

---
+++ backend/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { CacheModule } from './cache/cache.module';
+
+@Module({
+  imports: [DatabaseModule, CacheModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
